89164c04dfbe8417bd9d555cc358f0c30d073e4f
wnstjr <wnstjr8717@gmail.com>
2021-08-16 13:05:40 +0900

    sample refactoring

--- load_data.py
+++ load_data.py
@@ -16,34 +16,31 @@ class DatasetLoader:
         self.num_users = len(self.unique_users)
         self.user_to_index = {original: idx for idx, original in enumerate(self.unique_users)}
 
-        # 0 1 0 0 0 ... 0
-
         self.unique_apt = self.train_df.apt.unique()
         self.num_apt = len(self.unique_apt)
         self.apt_to_index = {original: idx for idx, original in enumerate(self.unique_apt)}
 
         self.val_df = val_temp_df[val_temp_df.user.isin(self.unique_users) & val_temp_df.apt.isin(self.unique_apt)]
 
-        self.train_df.rate = 1
-        self.val_df.rate = 1
-
     @staticmethod
     def read_data():
-        df = pd.read_csv(os.path.join(config['data_path'], 'rates.csv'))
+        df = pd.read_csv(os.path.join(config['data_path'], 'rates.csv'), usecols=['user', 'apt'])
         train_df, val_df = train_test_split(df, test_size=0.2, random_state=1234, shuffle=True)
+        train_df['searched'] = [1 for i in range(len(train_df))]
+        val_df['searched'] = [1 for i in range(len(val_df))]
         return train_df, val_df
 
     def generate_trainset(self):
         # user 0, 0, 0, 1,2, 3,3, -> movie: 0,0,0,0,0,0,
         x_train = pd.DataFrame({'user': self.train_df.user.map(self.user_to_index),
                                 'apt': self.train_df.apt.map(self.apt_to_index)})
-        y_train = self.train_df['rate'].astype(np.float32)
+        y_train = self.train_df['searched'].astype(np.float32)
         return x_train, y_train
 
     def generate_valset(self):
         x_val = pd.DataFrame({'user': self.val_df.user.map(self.user_to_index),
                               'apt': self.val_df.apt.map(self.apt_to_index)})
-        y_val = self.val_df['rate'].astype(np.float32)
+        y_val = self.val_df['searched'].astype(np.float32)
         return x_val, y_val
 
 
